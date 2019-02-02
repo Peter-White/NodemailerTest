b3d08edfcb1476b1f4b87458310b71a52c2e586f
FourLeafCoder <pwhitedeveloper@gmail.com>
2019-02-02 15:25:27 -0500

    fooking works

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
 
 
 const express = require('express');
-const config = require('./config/config');;
+const config = require('./config/config');
+
+'use strict';
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+ service: 'gmail',
+ auth: {
+        user: 'welotestemailsender@gmail.com',
+        pass: 'zX1c99NG'
+    }
+});
+
+const mailOptions = {
+  from: 'welotestemailsender@gmail.com', // sender address
+  to: 'pwhitedeveloper@gmail.com', // list of receivers
+  subject: 'Subject of your email', // Subject line
+  html: '<p>Works</p>'// plain text body
+};
+
+transporter.sendMail(mailOptions, function (err, info) {
+   if(err)
+     console.log(err)
+   else
+     console.log(info);
+});
 
 const app = express();
 
